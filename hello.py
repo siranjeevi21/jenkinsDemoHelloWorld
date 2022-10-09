eb1529fc45f337067ec50b993e2588717e02f612
Siranjeevi <66528036+siranjeevi21@users.noreply.github.com>
2022-10-09 18:42:57 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,3 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
+import streamlit as st
+st.title('Hello world - siranjeevi')
 
